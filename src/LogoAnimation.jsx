a9f9aca1b2fe78265dc9048d966151bca7994ef6
emmanuel540 <emmanuelhillary24@gmail.com>
2026-06-02 15:05:16 +0300

    updates

--- src/LogoAnimation.jsx
+++
@@ -1,8 +0,0 @@
-export default function LogoAnimation() {
-  return (
-    <div className="bg-logo" aria-hidden="true">
-      <div className="bg-logo__image bg-logo__bright" />
-      <div className="bg-logo__image bg-logo__faded" />
-    </div>
-  );
-}
